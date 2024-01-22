40fa19ae5b0085ae6f25fe17c338043e6ba33f4c
hello <joshthoo@gmail.com>
2024-01-22 15:04:57 +0800

    first router path

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,39 @@ package main
 
 import (
 	"fmt"
-	_ "log"
-	_ "net/http"
+	"log"
+	"net/http"
 
-	h "github.com/josh1248/cvwo-assignment-24-backend/repotest"
+	"github.com/gin-gonic/gin"
+
+	"github.com/josh1248/cvwo-assignment-24-backend/internal/database"
+	"github.com/josh1248/cvwo-assignment-24-backend/repotest"
 	// dependency check with go mod tidy.
 	// _ "github.com/lib/pq"
 )
 
+// rmb to capitalize for public access
+type message struct {
+	Msg string `json:"message"`
+}
+
+var m message = message{"hihihi!"}
+
+func throwGarbage(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, m)
+}
+
 func main() {
-	fmt.Print(h.Hello())
-	fmt.Print(h.Bye("hhh"))
+	fmt.Println(repotest.Hello(), "the package connections are working!")
+	db, err := database.ConnectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("%T", db)
+
+	fmt.Println("Test route now at http://localhost:8080")
+	router := gin.Default()
+	router.GET("/messages", throwGarbage)
+	router.Run("localhost:8080")
+
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
 // 127.0.0.1 refers to localhost
@@ -18,13 +21,18 @@ func ConnectToDB() (*sql.DB, error) {
 	db, err := sql.Open("postgres", secretCredentials)
 	//defer db.Close()
 	if err != nil {
+		log.Fatal(err)
 		return db, err
 	}
+	log.Println("Driver is running.")
 
 	err = db.Ping()
 	if err != nil {
+		log.Fatal(err)
 		return db, err
 	}
+
+	log.Println("Connected to database.")
 
 	return db, nil
 }
