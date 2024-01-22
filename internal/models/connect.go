c0b44ad57fe989ecfd92b99b7b77b0d3bb9852dd
hello <joshthoo@gmail.com>
2024-01-22 17:56:44 +0800

    test data

--- internal/models/connect.go
+++ internal/models/connect.go
@@ -8,12 +8,12 @@ import (
 
 func ConnectToDB() (*sql.DB, error) {
 	//switch between :memory: and a file directory for transient/permanent DBs.
-	db, err := sql.Open("sqlite", ":memory:")
+	db, err := sql.Open("sqlite", "internal/db/blogdb")
 	//not needed, since it will be closed upon interruption.
 	//defer db.Close()
 
 	if err != nil {
-		//possible shortening with named and naked returns.
+		//possible shortening with named and naked returns?
 		return db, err
 	}
 
@@ -22,5 +22,18 @@ func ConnectToDB() (*sql.DB, error) {
 		return db, err
 	}
 
-	return db, nil
+	createCommand := `
+		CREATE TABLE users (
+			id INT PRIMARY KEY,
+			name TEXT NOT NULL,
+			reputation INT NOT NULL
+		);
+	`
+
+	_, err = db.Exec(createCommand)
+	if err != nil {
+		return db, err
+	} else {
+		return db, nil
+	}
 }
